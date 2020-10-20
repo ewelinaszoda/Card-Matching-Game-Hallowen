7cd3492c149b14af80e26d269163b75db4cde38c
ewelinaszoda <ewelinaszoda@gmail.com>
2020-10-20 01:18:28 +0100

    Add checking match function to MixOrMatch Class

--- script.js
+++ script.js
@@ -95,5 +95,45 @@ flipCard(card) {
       }
   }
 }
+checkForCardMatch(card) {
+  if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+      this.cardMatch(card, this.cardToCheck);
+  else 
+      this.cardMismatch(card, this.cardToCheck);
+
+  this.cardToCheck = null;
+}
+cardMatch(card1, card2) {
+  this.matchedCards.push(card1);
+  this.matchedCards.push(card2);
+  card1.classList.add('matched');
+  card2.classList.add('matched');
+  this.audioController.match();
+  if(this.matchedCards.length === this.cardsArray.length)
+      this.victory();
+}
+cardMismatch(card1, card2) {
+  this.busy = true;
+  setTimeout(() => {
+      card1.classList.remove('visible');
+      card2.classList.remove('visible');
+      this.busy = false;
+  }, 1000);
+}
+shuffleCards(cardsArray) {
+  for (let i = cardsArray.length - 1; i > 0; i--) {
+      const randIndex = Math.floor(Math.random() * (i + 1));
+      [cardsArray[i], cardsArray[randIndex]] = [cardsArray[randIndex], cardsArray[i]];
+  }
+  cardsArray = cardsArray.map((card, index) => {
+      card.style.order = index;
+  });
+}
+getCardType(card) {
+  return card.getElementsByClassName('card-value')[0].src;
+}
+canFlipCard(card) {
+  return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+}
 
 }
